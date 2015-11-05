d4ca3929057cea4b66ce05b345161ab6febd3e6a
Hao Zhang <klaushaozhang.note@gmail.com>
2015-11-06 11:51:51 +1300

    Build Models

---
+++ MasterDetail/DataLayer/WorkOrderConfigration.cs
@@ -0,0 +1,31 @@
+﻿using MasterDetail.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace MasterDetail.DataLayer
+{
+  public class WorkOrderConfigration:EntityTypeConfiguration<WorkOrder>
+  {
+    public WorkOrderConfigration()
+    {
+      Property(wo => wo.OrderDateTime)
+        .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Computed);
+
+      Property(wo => wo.Description)
+        .HasMaxLength(256)
+        .IsOptional();
+
+      Property(wo => wo.Total)
+        .HasPrecision(18, 2)
+        .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Computed);
+
+      Property(wo => wo.CertificationRequirements)
+        .HasMaxLength(120)
+        .IsOptional();
+    }
+  }
+}
---
+++ MasterDetail/Models/ServiceItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MasterDetail.Models
+{
+  public class ServiceItem
+  {
+    public int ServiceItemItem { get; set; }
+    public string ServiceItemCode { get; set; }
+    public string ServiceItemName { get; set; }
+    public decimal Rate { get; set; }
+
+  }
+}
